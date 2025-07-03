9639e3878482df755261eae7bdc8d6d8d2011899
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-03 16:47:27 +0900

    feat: 포트번호 3000, 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }], // '@'를 '/src' 폴더로 대체
+  },
+  server: {
+    port: 3000,
+  },
 });
