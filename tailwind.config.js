2b559c48f784aa0d8413a1b6d9cd0317b3b58107
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-04 02:32:56 +0900

    feat: Tailwind 설정 및 토큰 경로 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+import tokens from './src/tokens/design-tokens.json' assert { type: 'json' };
+
 export default {
     content: [
         './index.html',
         './src/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: tokens.colors,
+            fontSize: tokens.fontSizes,
+        },
     },
     plugins: [],
 }
