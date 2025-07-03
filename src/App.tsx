e627b8ee500ae76f487836ee3fc5e3118b438086
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-03 17:39:20 +0900

    feat: tailwind CSS 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-blue-100 text-3xl font-bold'>
+      Chikahae
+    </div>
+  );
 }
 
 export default App;
