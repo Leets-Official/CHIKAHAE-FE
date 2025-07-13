28dca3f30932016d013f469efe8984f1d92984d5
thisminn <noblesse0308@naver.com>
2025-07-13 20:57:24 +0900

    Test Page 병합

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
-import ToastTest from './pages/test/ToastTest';
-import NavTest from './pages/test/NavTest';
-import RadioButtonTest from './pages/test/RadioButtonTest';
-import TextButtonTest from './pages/test/TextButtonTest';
+import ComponentTest from './pages/test/component-test';
+import AllComponentsTest from './pages/test/AllComponentTest';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/toast-test' element={<ToastTest />} />
-        <Route path='/nav-test' element={<NavTest />} />
-        <Route path='/radio-test' element={<RadioButtonTest />} />
-        <Route path='/textbutton-test' element={<TextButtonTest />} />
+        <Route path='/tests/all' element={<AllComponentsTest />} />
       </Routes>
     </BrowserRouter>
   );
