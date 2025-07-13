77669e52b7e6b98abe7be84cbc9e2e74654d14e1
thisminn <noblesse0308@naver.com>
2025-07-14 00:00:09 +0900

    feat: 이미지 파일 수정

--- src/components/ui/Nav/GlobalTopNav.tsx
+++ src/components/ui/Nav/GlobalTopNav.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { ReactComponent as CancelIcon } from '@/assets/images/cancel.svg';
-import { ReactComponent as LeftIcon } from '@/assets/images/cancel.svg';
+import { ReactComponent as LeftIcon } from '@/assets/images/Chevron_left.svg';
 
 export const GlobalTopNav = () => {
   return (
