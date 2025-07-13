2acee34ecce6a8532851acde642fd18fd3aa39b1
thisminn <noblesse0308@naver.com>
2025-07-13 23:36:31 +0900

    TopNav 기능 별 분류 및 HomeTopNav css 적용

---
+++ src/components/ui/Nav/GlobalTopNav.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import { ReactComponent as CancelIcon } from '@/assets/images/cancel.svg';
+import { ReactComponent as LeftIcon } from '@/assets/images/cancel.svg';
+
+export const GlobalTopNav = () => {
+  return (
+    <nav
+      className={`
+                fixed top-0 left-0 right-0
+                flex h-14 px-4
+                justify-between
+                items-center
+                bg-bg-tertiary-gray:#FAFBFC;
+                w-full`}
+    >
+      <Link to='/'>
+        <LeftIcon className='h-[30px] w-[30px]' />
+      </Link>
+      <Link to='/'>
+        <div className='text-fg-primary text-[16px]'>기록하기 </div>
+      </Link>
+      <Link to='/'>
+        <CancelIcon className='h-[30px] w-[30px]' />
+      </Link>
+    </nav>
+  );
+};
+
+export default GlobalTopNav;
---
+++ src/components/ui/Nav/HomeTopNav.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import { ReactComponent as GearIcon } from '@/assets/images/gear.svg';
+import { ReactComponent as BellIcon } from '@/assets/images/bell.svg';
+
+export const HomeTopNav = () => {
+  return (
+    <nav
+      className={`
+            fixed top-0 left-0 right-0
+            flex h-20 px-6
+            justify-between
+            items-center
+            bg-bg-tertiary-gray: #FAFBFC;
+            w-full`}
+    >
+      <Link to='/'>
+        <div className='text-fg-primary-blue'>치카해</div>
+      </Link>
+      <div
+        className={`flex px-2 m-0 justify-between items-center bg-bg-tertiary-gray:#FAFBFC w-[100px]`}
+      >
+        <Link to='/'>
+          <GearIcon className='h-[30px] w-[30px]' />
+        </Link>
+        <Link to='/'>
+          <BellIcon className='h-[30px] w-[30px]' />
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default HomeTopNav;
