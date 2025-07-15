a58098c7234846b7e1a5180eb73a4cf86491835e
thisminn <noblesse0308@naver.com>
2025-07-15 23:20:27 +0900

    feat:GlobalTopNav 조건부 css 적용 아이콘 사이즈 수정

--- src/components/ui/Nav/GlobalTopNav.tsx
+++ src/components/ui/Nav/GlobalTopNav.tsx
@@ -2,54 +2,45 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as CancelIcon } from '@/assets/images/cancel.svg';
 import { ReactComponent as LeftIcon } from '@/assets/images/chevron_left.svg';
 
-type NavState = 'center' | 'left';
-
 interface GlobalTopNavProps {
-  navState: NavState;
+  message: string;
+  isCenter?: boolean;
 }
 
-export const GlobalTopNav = ({ navState }: GlobalTopNavProps) => {
-  const isCenter = navState === 'center';
+export const GlobalTopNav = ({ isCenter, message = '기록하기' }: GlobalTopNavProps) => {
+  const NavItem = () => {
+    return (
+      <>
+        <Link to='/'>
+          <LeftIcon />
+        </Link>
+        <div className='text-fg-primary text-[16px]'>{message}</div>
+      </>
+    );
+  };
 
   return (
-    <nav
+    <div
       className={`
                 fixed top-0 left-0 right-0
                 flex h-14 px-4
                 items-center
-                bg-bg-tertiary-gray:#FAFBFC;
+                bg-bg-tertiary-gray:#FAFBFC
                 w-full
                 justify-between`}
     >
       {isCenter ? (
-        <>
-          <Link to='/'>
-            <LeftIcon className='h-[30px] w-[30px]' />
-          </Link>
-          <Link to='/'>
-            <div className='text-fg-primary text-[16px]'>기록하기 </div>
-          </Link>
-          <Link to='/'>
-            <CancelIcon className='h-[30px] w-[30px]' />
-          </Link>
-        </>
+        <NavItem />
       ) : (
-        <>
-          <div className='flex items-center justify-center gap-6'>
-            <Link to='/'>
-              <LeftIcon className='h-[30px] w-[30px]' />
-            </Link>
-            <Link to='/'>
-              <div className='text-fg-primary text-[16px]'>기록하기 </div>
-            </Link>
-          </div>
-          <Link to='/'>
-            <CancelIcon className='h-[30px] w-[30px]' />
-          </Link>
-        </>
+        <div className='flex gap-[20px]'>
+          <NavItem />
+        </div>
       )}
-    </nav>
+
+      <Link to='/'>
+        <CancelIcon className='h-[30px] w-[30px]' />
+      </Link>
+    </div>
   );
 };
-
 export default GlobalTopNav;
