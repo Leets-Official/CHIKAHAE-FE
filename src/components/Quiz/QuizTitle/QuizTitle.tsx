31f0bf5758caf41e108d2541b4bb5b879469d80a
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-16 14:25:27 +0900

    feat: quiz - title 컴포넌트 구현

---
+++ src/components/Quiz/QuizTitle/QuizTitle.tsx
@@ -0,0 +1,16 @@
+import type { QuizTitleProps } from './QuizTitle.types';
+
+const QuizTitle = ({ questionNumber, questionText }: QuizTitleProps) => {
+  return (
+    <div className='flex w-[360px] px-[30px] flex-col items-center gap-2 text-center'>
+      <div className='body-16-b text-[#2593BD]' style={{ lineHeight: '19px' }}>
+        {questionNumber}번 문제
+      </div>
+      <div className='head-24-eb text-black' style={{ lineHeight: '38px' }}>
+        {questionText}
+      </div>
+    </div>
+  );
+};
+
+export default QuizTitle;
