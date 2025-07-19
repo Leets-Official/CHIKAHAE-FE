6a584d076e10dbc7c11b627d713d53928e655c98
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-20 04:51:23 +0900

    refactor: 모달에서 footer 영역 기본값으로 버튼 설정, 오버레이 미적용 문제 수정

--- src/components/ui/Modal/ModalPortal.tsx
+++ src/components/ui/Modal/ModalPortal.tsx
@@ -1,13 +1,14 @@
 // src/components/common/Modal/ModalPortal.tsx
-import React from "react";
-import { createPortal } from "react-dom";
+import React from 'react';
+import { createPortal } from 'react-dom';
 
 interface ModalPortalProps {
   children: React.ReactNode;
 }
 
 const ModalPortal = ({ children }: ModalPortalProps) => {
-  return createPortal(children, document.body);  // 임시로 body에 직접 렌더링
+  const modalRoot = document.getElementById('modal-root')!;
+  return createPortal(children, modalRoot);
 };
 
 export default ModalPortal;
