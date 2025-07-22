65d157bd068100a534980deee33adc813b7ca01e
thisminn <noblesse0308@naver.com>
2025-07-22 10:25:46 +0900

    refactor: 카카오 로그인 아이콘 구현 방법 변경

--- src/components/ui/Button/KakaoLoginButton.tsx
+++ src/components/ui/Button/KakaoLoginButton.tsx
@@ -1,5 +1,3 @@
-import { ReactComponent as KakaoLogin } from '@/assets/icons/loginIcon.svg';
-
 const KakaoLoginButton = () => {
   const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
   const REDIRECT_URL = 'http://localhost:3000/auth/kakao/callback';
@@ -11,8 +9,19 @@ const KakaoLoginButton = () => {
   };
 
   return (
-    <button onClick={handleLogin} className='flex items-center justify-center w-full'>
-      <KakaoLogin />
+    <button
+      onClick={handleLogin}
+      className='
+    flex items-center justify-center 
+    bg-bg-primary-yellow
+    h-12 rounded-lg 
+    w-full
+    gap-2
+    
+    '
+    >
+      <img src='src/assets/icons/KaKaoSymbol.svg' alt='KakaoIcon'></img>
+      카카오로 시작하기
     </button>
   );
 };
