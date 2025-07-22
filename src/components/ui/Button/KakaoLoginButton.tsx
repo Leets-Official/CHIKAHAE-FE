7742abda49b44ede8c87319a0221c08ea4184d84
thisminn <noblesse0308@naver.com>
2025-07-22 13:00:53 +0900

    feat: 카카오 로그인 도메인 환경변수 설정 gitignore에 .env.* 행 추가

--- src/components/ui/Button/KakaoLoginButton.tsx
+++ src/components/ui/Button/KakaoLoginButton.tsx
@@ -1,6 +1,6 @@
 const KakaoLoginButton = () => {
   const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
-  const REDIRECT_URL = 'http://localhost:3000/auth/kakao/callback';
+  const REDIRECT_URL = import.meta.env.VITE_KAKAO_REDIRECT_URL;
 
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code`;
 
