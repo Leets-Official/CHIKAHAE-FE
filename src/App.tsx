2c68f958e57b86a74eeedfa710ce0437c4d73111
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-23 01:54:09 +0900

    feat: 양치 시작 화면 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import AllComponentsTest from './pages/test/AllComponentTest';
-import StartQuizPage from '@/pages/quiz/QuizStartPage';
+import QuizStartPage from '@/pages/quiz/QuizStartPage';
 import QuizPage from '@/pages/quiz/QuizPage';
+import BrushingStartPage from './pages/brushing/BrushingStartPage';
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/tests/all' element={<AllComponentsTest />} />
-        <Route path='/quiz/start' element={<StartQuizPage />} />
+        <Route path='/quiz/start' element={<QuizStartPage />} />
         <Route path='/quiz' element={<QuizPage />} />
+        <Route path='/brush/start' element={<BrushingStartPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/ui/NoticeList.tsx
+++ src/components/ui/NoticeList.tsx
@@ -6,10 +6,10 @@ type NoticeListProps = {
 
 const NoticeListBox = ({ noticeList }: NoticeListProps) => {
   return (
-    <div className='inline-flex items-center px-[15px] pr-[49px] py-[25px] rounded-[8px] border-[2px] border-bg-primary-gray bg-fg-veryweak'>
+    <div className='inline-flex items-center pt-[30px] pr-[48px] pb-[30px] pl-[16px] rounded-[8px] border-[2px] border-bg-primary-gray bg-fg-veryweak'>
       <ul className='list-disc pl-[16px] space-y-3'>
         {noticeList.map((text, index) => (
-          <li key={index} className='body-12-r text-left text-fg-verystrong'>
+          <li key={index} className='body-14-r text-left text-fg-verystrong'>
             {text}
           </li>
         ))}
