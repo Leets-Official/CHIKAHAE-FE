6e5ed322b90508aa1ee11b59f35baa7be9f1a845
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-24 00:40:52 +0900

    chore: merge develop into fix/#42-build-error

---
+++ src/pages/auth/LoginPage.tsx
@@ -0,0 +1,24 @@
+import { ReactComponent as LoginLogo } from '@/assets/icons/homeLogo.svg';
+import KakaoLoginButton from '@/components/ui/Button/KakaoLoginButton';
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center gap-y-28'>
+        <LoginLogo />
+        <KakaoLoginButton />
+      </div>
+
+      <div
+        className={`fixed bottom-10 left-1/2 -translate-x-1/2 w-full max-w-[480px] min-w-[360px] px-4 py-2.5`}
+      >
+        <p className='flex flex-col items-center justify-center font-bold text-[10px] leading-[11px] tracking-[-0.1px] text-fg-gray-weak'>
+          <span>가입하면 CHIKAHE의 </span>
+          <span>이용약관 및 개인정보처리방침에 동의하게 됩니다.</span>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
