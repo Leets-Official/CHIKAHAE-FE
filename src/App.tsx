83a4d79a6fa38f19f857d2f47ab6a3d7850442ba
eun-seoo <kimeunseo040@gamil.com>
2025-07-23 23:45:32 +0900

    refactor: 루트 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,20 @@ import QuizPage from '@/pages/quiz/QuizPage';
 import MyPage from '@/pages/mypage/MyPage';
 import MyPageEdit from '@/pages/mypage/EditProfilePage';
 import EditFieldPage from '@/pages/mypage/EditFieldPage';
-import ChikaAlramPage from '@/pages/mypage/ChikaAlramPage';
-import TimePickerPage from '@/pages/mypage/TimePickerPage';
+import ChikaAlarmPage from '@/pages/mypage/ChikaAlarmPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>  
+      <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/tests/all' element={<AllComponentsTest />} />
         <Route path='/quiz/start' element={<StartQuizPage />} />
         <Route path='/quiz' element={<QuizPage />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='/edit' element={<MyPageEdit />} />
-        <Route path="/edit/:field" element={<EditFieldPage />} />
-        <Route path="/mypage/alram" element={<ChikaAlramPage />} />
-        <Route path="/mypage/timePicker" element={<TimePickerPage />} />
+        <Route path='/edit/:field' element={<EditFieldPage />} />
+        <Route path='/mypage/alarm' element={<ChikaAlarmPage />} />
       </Routes>
     </BrowserRouter>
   );
