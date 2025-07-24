0ba525226206f0c9c98f88999de39236e56ffa80
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-25 00:25:40 +0900

    feat: 퀴즈 조회, 정답 제출, 결과 조회 api 연결

--- src/pages/quiz/QuizStartPage.tsx
+++ src/pages/quiz/QuizStartPage.tsx
@@ -1,9 +1,28 @@
 import StartPageTemplate from '@/features/start/StartPageTemplate';
 import image from '@/assets/images/quizStart.png';
 import { useNavigate } from 'react-router-dom';
+import { fetchTodayQuiz } from '@/api/quiz/quizAPI';
+import { useState } from 'react';
 
 const QuizStartPage = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const handleStart = async () => {
+    try {
+      setLoading(true);
+      const data = await fetchTodayQuiz();
+      if (data.success) {
+        navigate('/quiz', { state: data.data }); // 퀴즈 데이터 넘기기
+      } else {
+        console.error(data.error?.message || '퀴즈 데이터를 불러오지 못했습니다.');
+      }
+    } catch (error) {
+      console.error('퀴즈 요청 중 오류가 발생했습니다.', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <StartPageTemplate
@@ -13,7 +32,8 @@ const QuizStartPage = () => {
       title='매일 퀴즈 도전하기'
       coinText='획득 치카코인'
       noticeList={['문항 수', '객관식, OX', '중도 이탈', '결과 확인']}
-      onStart={() => navigate('/quiz')}
+      onStart={handleStart}
+      startButtonText={loading ? '불러오는 중...' : '시작하기'} // 임시
     />
   );
 };
