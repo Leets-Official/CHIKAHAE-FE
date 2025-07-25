0a4cbb472661a3aa0db9778c8e0434408b3b8aa4
eun-seoo <kimeunseo040@gamil.com>
2025-07-25 14:43:27 +0900

    fix: homeLogo에 사이즈 지정

--- src/components/ui/Nav/HomeTopNav.tsx
+++ src/components/ui/Nav/HomeTopNav.tsx
@@ -5,9 +5,9 @@ import { ReactComponent as HomeLogo } from '@/assets/icons/homeLogo.svg';
 export const HomeTopNav = () => {
   return (
     <div className="fixed left-1/2 -translate-x-1/2 w-[360px] h-[44px] flex flex-row items-center justify-between gap-[10px] p-[10px_20px] bg-white opacity-100 rotate-0">
-      <HomeLogo className="w-[64.384px] h-[20.798px] opacity-100 rotate-0" />
+      <HomeLogo className='w-[64.384px] h-[20.798px]' />
       <Link to="/">
-        <BellIcon className="w-6 h-6 opacity-100 rotate-0" />
+        <BellIcon className="w-6 h-6" />
       </Link>
     </div>
   );
