6e7bb93f125dc2978acc6ba95ea467180dd41b53
eun-seoo <kimeunseo040@gamil.com>
2025-07-25 11:53:38 +0900

    refactor: HomeTopNav 스타일 수정

--- src/components/ui/Nav/HomeTopNav.tsx
+++ src/components/ui/Nav/HomeTopNav.tsx
@@ -1,32 +1,15 @@
 import { Link } from 'react-router-dom';
-import { ReactComponent as GearIcon } from '@/assets/icons/gear.svg';
 import { ReactComponent as BellIcon } from '@/assets/icons/bell.svg';
+import { ReactComponent as HomeLogo } from '@/assets/icons/homeLogo.svg';
 
 export const HomeTopNav = () => {
   return (
-    <nav
-      className={`
-            fixed top-0 left-1/2 -translate-x-1/2
-            flex h-20 px-6
-            justify-between
-            items-center
-            bg-bg-tertiary-gray
-            w-full  max-w-[480px] min-w-[360px]`}
-    >
-      <Link to='/'>
-        <div className='text-fg-primary-blue body-16-eb'>치카해</div>
+    <div className="fixed left-1/2 -translate-x-1/2 w-[360px] h-[44px] flex flex-row items-center justify-between gap-[10px] p-[10px_20px] bg-white opacity-100 rotate-0">
+      <HomeLogo className="w-[64.384px] h-[20.798px] opacity-100 rotate-0" />
+      <Link to="/">
+        <BellIcon className="w-6 h-6 opacity-100 rotate-0" />
       </Link>
-      <div
-        className={`flex px-2 m-0 justify-between items-center bg-bg-tertiary-gray w-[100px]`}
-      >
-        <Link to='/'>
-          <BellIcon />
-        </Link>
-        <Link to='/'>
-          <GearIcon />
-        </Link>
-      </div>
-    </nav>
+    </div>
   );
 };
 
