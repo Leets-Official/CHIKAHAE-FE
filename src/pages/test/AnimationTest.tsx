8a4242266f03d98a227fda88be34ec6af3219d6a
thisminn <noblesse0308@naver.com>
2025-07-25 16:42:32 +0900

    feat: 타이머, tip 부분 배치(임시)

--- src/pages/test/AnimationTest.tsx
+++ src/pages/test/AnimationTest.tsx
@@ -1,13 +1,29 @@
 import LottieTest from '@/components/LottieTest';
 import Timer from '@/components/ui/Timer/Timer';
+import { ReactComponent as AnimationBackground } from '@/assets/images/animationBackground.svg';
+import GlobalTopNav from '@/components/ui/Nav/GlobalTopNav';
 
 const Page = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-y-15 max-w-[480px] min-w-[360px]'>
-      <h1>치카해 애니메이션 테스트중입니다</h1>
-      <Timer />
-      <LottieTest />
-    </div>
+    <>
+      <GlobalTopNav type='global' showCancel={false} message='양치' />
+      <div className='flex flex-col items-center justify-center min-h-screen gap-15 relative z-0 px-4'>
+        {/* 1. 타이머 */}
+        <Timer/>
+
+        {/* 2. 배경 + 애니메이션 묶음 */}
+        <div className='relative flex justify-center items-center max-w-[480px] min-w-[360px] w-full'>
+          {/* background는 absolute지만 부모 relative 기준 */}
+          <AnimationBackground className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full not-visited:lz-0' />
+          <div className='relative z-10 flex justify-center items-center pt-10'>
+            <LottieTest />
+          </div>
+        </div>
+
+        {/* 3. TIP 문구 */}
+        <p className='text-center text-base font-semibold'>tip: 양치 매일 하기</p>
+      </div>
+    </>
   );
 };
 
