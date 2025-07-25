e0837d2d40df5bef140ab82b6acb64a13ae82ed6
thisminn <noblesse0308@naver.com>
2025-07-25 14:45:47 +0900

    timer 추가

--- src/pages/test/AnimationTest.tsx
+++ src/pages/test/AnimationTest.tsx
@@ -1,9 +1,11 @@
 import LottieTest from '@/components/LottieTest';
+import Timer from '@/components/ui/Timer/Timer';
 
 const Page = () => {
   return (
     <div className='flex flex-col items-center justify-center gap-y-15 max-w-[480px] min-w-[360px]'>
       <h1>치카해 애니메이션 테스트중입니다</h1>
+      <Timer />
       <LottieTest />
     </div>
   );
