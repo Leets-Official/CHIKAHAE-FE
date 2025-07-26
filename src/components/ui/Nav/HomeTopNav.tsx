274e0a5e81a5106597513a6eaef5bdaae92212e1
nabbang6 <hawwah03@gachon.ac.kr>
2025-07-27 05:40:16 +0900

    refactor: Home Nav CSS 구조 개선

--- src/components/ui/Nav/HomeTopNav.tsx
+++ src/components/ui/Nav/HomeTopNav.tsx
@@ -4,13 +4,11 @@ import { ReactComponent as HomeLogo } from '@/assets/icons/homeLogo.svg';
 
 export const HomeTopNav = () => {
   return (
-    <div className="fixed w-full max-w-[480px] min-w-[360px] h-[44px] flex items-center justify-center">
-      <div className="w-[360px] h-[24px] flex flex-row items-center justify-between gap-4 rotate-0 mx-auto"> 
-        <HomeLogo />
-        <Link to="/">
-          <BellIcon />
-        </Link>
-      </div>
+    <div className='fixed top-0 left-1/2 -translate-x-1/2 w-full max-w-[480px] min-w-[360px] p-[10px_20px] h-14 flex items-center justify-between bg-bg-tertiary-gray z-999'>
+      <HomeLogo />
+      <Link to='/'>
+        <BellIcon />
+      </Link>
     </div>
   );
 };
