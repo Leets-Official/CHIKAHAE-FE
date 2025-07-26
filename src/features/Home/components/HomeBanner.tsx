ed30023b677b49e13f4a27764eb9cdd7b50238a5
eun-seoo <kimeunseo040@gamil.com>
2025-07-26 16:01:31 +0900

    style: HomeBanner 컴포넌트 스타일 및 구조 개선

--- src/features/Home/components/HomeBanner.tsx
+++ src/features/Home/components/HomeBanner.tsx
@@ -6,7 +6,7 @@ import { ReactComponent as Coin } from '@/assets/icons/coin.svg';
 const HomeBanner = () => {
   return (
     <div className="relative w-[360px] h-[340px]">
-      <Background className="absolute inset-0 w-full h-full object-cover z-0" />
+      <Background className="absolute inset-0 w-full max-w-[480px] min-w-[360px] h-full object-cover z-0" />
       <Rabbit className="absolute w-[71.12px] h-[120px] top-[208.51px] left-[145.44px] z-10" />
       <Toothbrush className="absolute top-[165.33px] left-[85.43px] z-10" />
       <div className="absolute w-[86.18px] h-[36px] top-[20.67px] left-[253.49px] z-10 flex items-center justify-center">
