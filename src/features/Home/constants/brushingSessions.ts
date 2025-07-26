d1511d7524f5600beef2ae27212a9d71b2bdd5fc
eun-seoo <kimeunseo040@gamil.com>
2025-07-26 16:03:49 +0900

    refactor: brushingCards에 buttonText 필드 추가

--- src/features/Home/constants/brushingSessions.ts
+++ src/features/Home/constants/brushingSessions.ts
@@ -3,6 +3,7 @@ export interface BrushingCardConfig {
   title: string;
   description: string;
   route?: string;
+  buttonText: string;
 }
 
 export const brushingCards: BrushingCardConfig[] = [
@@ -10,21 +11,25 @@ export const brushingCards: BrushingCardConfig[] = [
     id: 'morning',
     title: '아침 양치하기',
     description: '하루를 상쾌하게',
+    buttonText: '양치하러 가기',
   },
   {
     id: 'lunch',
     title: '점심 양치하기',
     description: '점심먹고 땡 !',
+    buttonText: '양치하러 가기',
   },
   {
     id: 'dinner',
     title: '저녁 양치하기',
     description: '하루를 마무리',
+    buttonText: '양치하러 가기',
   },
   {
     id: 'quiz',
     title: '오늘의 퀴즈',
     description: '도전해볼까요 !?',
     route: '/quiz/start',
+    buttonText: '퀴즈 도전 하기',
   },
 ];
