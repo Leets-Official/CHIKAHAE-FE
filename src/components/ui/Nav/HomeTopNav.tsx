854221768592bc61a01979a7e7310574cded8571
eun-seoo <kimeunseo040@gamil.com>
2025-07-26 21:16:42 +0900

    fix: HomeTopNav 레이아웃 수정

--- src/components/ui/Nav/HomeTopNav.tsx
+++ src/components/ui/Nav/HomeTopNav.tsx
@@ -4,11 +4,11 @@ import { ReactComponent as HomeLogo } from '@/assets/icons/homeLogo.svg';
 
 export const HomeTopNav = () => {
   return (
-    <div className="fixed left-1/2 -translate-x-1/2 w-full max-w-[480px] min-w-[360px] h-[44px] flex flex-row items-center justify-between gap-[10px] p-[10px_20px] rotate-0">
-      <div className="w-[320px] h-[24px] flex flex-row justify-between rotate-0 mx-auto"> 
+    <div className="fixed w-full max-w-[480px] min-w-[360px] h-[44px] flex items-center justify-center">
+      <div className="w-[320px] h-[24px] flex flex-row items-center justify-between gap-4 rotate-0 mx-auto"> 
         <HomeLogo />
         <Link to="/">
-          <BellIcon className="w-6 h-6" />
+          <BellIcon />
         </Link>
       </div>
     </div>
