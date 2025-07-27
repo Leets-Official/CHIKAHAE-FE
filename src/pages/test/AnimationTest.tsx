c7d4343f5ea903124d0e85ef4d196c1fe3df33ba
thisminn <noblesse0308@naver.com>
2025-07-27 16:13:49 +0900

    애니메이션 전용 타이머 타입 설정 및 적용 테스트용 배경 적용

--- src/pages/test/AnimationTest.tsx
+++ src/pages/test/AnimationTest.tsx
@@ -1,9 +1,9 @@
 import LottieTest from '@/components/LottieTest';
 import Timer from '@/components/ui/Timer/Timer';
-import { ReactComponent as AnimationBackground } from '@/assets/images/animationBackground.svg';
 import GlobalTopNav from '@/components/ui/Nav/GlobalTopNav';
 import { TIPS } from '@/constants/tips';
 import { useState, useEffect } from 'react';
+import { ReactComponent as BackgroundImage } from '@/assets/images/backgroundImage.svg';
 
 const Page = () => {
   const [tipIndex, setTipIndex] = useState(0);
@@ -24,16 +24,16 @@ const Page = () => {
   return (
     <>
       <GlobalTopNav type='global' showCancel={false} message='양치' />
-      <div className='flex flex-col items-center justify-center min-h-screen gap-15 relative z-0 px-4'>
-        <Timer duration={180} />
-        <div className='relative flex justify-center items-center max-w-[480px] min-w-[360px] w-full'>
-          <AnimationBackground className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full not-visited:lz-0' />
+      <div className='flex flex-col items-center justify-center min-h-screen gap-10 relative z-0 px-4'>
+        <Timer duration={180} size='wide' />
+        <div className='relative flex justify-center items-center max-w-[480px] min-w-[360px] w-full min-h-[450px]'>
+          <BackgroundImage className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full not-visited:lz-0' />
           <div className='relative z-10 flex justify-center items-center pt-10'>
             <LottieTest />
           </div>
         </div>
 
-        <p className='text-center text-[16px] leading-[22px] tracking-[-0.16px] font-extrabold'>
+        <p className='text-center text-[16px] leading-[22px] tracking-[-0.16px] font-extrabold z-9999'>
           {TIPS[tipIndex]}
         </p>
       </div>
