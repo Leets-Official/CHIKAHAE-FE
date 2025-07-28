5a33a157acff7c4d7cefb153de9d40a913aa87bc
eun-seoo <kimeunseo040@gamil.com>
2025-07-29 01:48:38 +0900

    fix: App.tsx 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import QuizPage from '@/pages/quiz/QuizPage';
 import SignupPage from './pages/auth/SignupPage';
 import QuizStartPage from '@/pages/quiz/QuizStartPage';
 import BrushingStartPage from './pages/brushing/BrushingStartPage';
+import AnimationTest from './pages/test/AnimationTest';
 import MyPage from '@/pages/mypage/MyPage';
 import MyPageEdit from '@/pages/mypage/EditProfilePage';
 import ChikaAlarmPage from '@/pages/mypage/ChikaAlarmPage';
@@ -22,6 +23,7 @@ function App() {
         <Route path='/quiz/start' element={<QuizStartPage />} />
         <Route path='/quiz' element={<QuizPage />} />
         <Route path='/brush/start' element={<BrushingStartPage />} />
+        <Route path='/animation' element={<AnimationTest />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='/edit' element={<MyPageEdit />} />
         <Route path='/mypage/alarm' element={<ChikaAlarmPage />} />
