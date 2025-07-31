967af849a6bf87b16dc642200565f6f7206b380c
eun-seoo <kimeunseo040@gamil.com>
2025-07-31 18:03:41 +0900

    feat:store 컴포넌트 테스트

---
+++ src/pages/test/StoreItemTest.tsx
@@ -0,0 +1,11 @@
+import { StoreItemList } from '@/features/Store/storeItems/StoreItemList';
+
+const StoreItemTest: React.FC = () => {
+  return (
+    <div>
+      <StoreItemList />
+    </div>
+  );
+};
+
+export default StoreItemTest;
