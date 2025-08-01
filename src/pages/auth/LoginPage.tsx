7ec23f3c98513b2e040b6519de81142addd6239e
nabbang6 <hawwah03@gachon.ac.kr>
2025-08-01 13:51:08 +0900

    refactor: 로그인 페이지 - 캐릭터 랜덤 메시지 기능 분리

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -4,13 +4,14 @@ import KakaoLoginButton from '@/components/ui/Button/KakaoLoginButton';
 import { CHARACTER_MESSAGES } from '@/constants/characterMessages';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import RandomCharacterMessage from '@/features/login/RandomCharcterMessage';
 
 const LoginPage = () => {
   const prevStoredIndex = Number(sessionStorage.getItem('prevRandomIndex') ?? -1);
   const [randomIndex, setRandomIndex] = useState<number | null>(null);
   const navigate = useNavigate();
 
-  // 토큰 이용해서 로그인 여부 판단 -> login 페이지 이동 시 home으로 이동시킴
+  // 토큰이 있을 경우 홈으로 이동
   useEffect(() => {
     const token = localStorage.getItem('accessToken');
     if (token) {
@@ -18,6 +19,7 @@ const LoginPage = () => {
     }
   }, [navigate]);
 
+  // 랜덤 인덱스를 선택하여 중복 표시 방지
   useEffect(() => {
     let newIndex;
     do {
@@ -28,10 +30,10 @@ const LoginPage = () => {
     sessionStorage.setItem('prevRandomIndex', newIndex.toString());
   }, []);
 
+  // 인덱스가 아직 설정되지 않았으면 null 반환
   if (randomIndex === null) {
     return null;
   }
-  const { main, sub, Svg } = CHARACTER_MESSAGES[randomIndex];
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen'>
@@ -40,27 +42,15 @@ const LoginPage = () => {
         alt='Signup Background'
         className='absolute inset-0 w-full h-full object-cover'
       />
-      {/*  */}
 
       <div className='absolute top-0 left-1/2 -translate-x-1/2 max-w-[480px] min-w-[360px] h-full z-10'>
         <SignupSideLogo className='absolute top-7 right-5' />
       </div>
 
       <div className='absolute top-30 bottom-0 left-1/2 -translate-x-1/2 flex flex-col justify-between items-center w-full max-w-[480px] min-w-[360px] z-20 px-5 pb-5'>
-        <div className='flex flex-col gap-3.25 items-center pt-5'>
-          <p className='text-fg-accent-blue-weak text-[26px] font-extrabold leading-[30px] tracking-[-0.26px]'>
-            {main}
-          </p>
-          <p className='text-[16px] font-bold tracking-[-0.16px] text-fg-strong leading-none'>
-            <span className='block'>{sub}</span>
-            <span className='block'></span>
-          </p>
-        </div>
+        <RandomCharacterMessage index={randomIndex} />
 
-        <div className='flex flex-col items-center'>
-          <Svg />
-        </div>
-        <div className='flex flex-col items-center gap-y-4  max-w-[480px] min-w-[360px] px-5'>
+        <div className='flex flex-col items-center gap-y-4 max-w-[480px] min-w-[360px] px-5'>
           <KakaoLoginButton />
           <p className='flex flex-col items-center justify-center font-bold text-[10px] leading-[11px] tracking-[-0.1px] text-fg-medium'>
             <span>가입하면 CHIKAHAE의 </span>
