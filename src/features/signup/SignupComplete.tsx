24561d11203a2a7e355ed55828c0521c229a9d9d
nabbang6 <hawwah03@gachon.ac.kr>
2025-08-01 13:52:16 +0900

    style: tailwind 클래스 정렬로 스타일 구조 통일

--- src/features/signup/SignupComplete.tsx
+++ src/features/signup/SignupComplete.tsx
@@ -15,45 +15,13 @@ const SignupComplete = () => {
           className='absolute inset-0 w-full h-full object-cover'
         />
         <div className='flex flex-col min-h-screen justify-between pt-11'>
-          <div
-            className='
-        flex               
-        flex-col         
-        gap-y-10           
-        px-5         
-        mt-10
-        w-full
-        max-w-[480px]
-        min-w-[360px]
-        mx-auto          
-        z-10 
-      '
-          >
+          <div className='flex flex-col gap-y-10 px-5 mt-10 w-full max-w-[480px] min-w-[360px] mx-auto z-10'>
             <div className='w-full text-left'>
-              <p
-                className='
-              text-fg-primary
-              text-[22px]
-              font-extrabold
-              leading-[25px]
-              tracking-[-0.22px]
-              self-stretch
-              mb-2.5 
-            '
-              >
+              <p className='text-fg-primary text-[22px] font-extrabold leading-[25px] tracking-[-0.22px] self-stretch mb-2.5'>
                 치카해 가입이 완료되었어요!
               </p>
 
-              <p
-                className='
-              text-fg-primary
-              text-[14px]
-              font-normal
-              leading-[16px]
-              tracking-[-0.14px]
-              items-stretch
-            '
-              >
+              <p className='text-fg-primary text-[14px] font-normal leading-[16px] tracking-[-0.14px] items-stretch'>
                 이제 치카해의 기능을 모두 이용할 수 있어요.
               </p>
             </div>
@@ -64,11 +32,7 @@ const SignupComplete = () => {
           </div>
         </div>
         <div
-          className={`
-          fixed bottom-0 left-1/2 -translate-x-1/2 
-           max-w-[480px] min-w-[360px] 
-          px-4 py-2.5
-          `}
+          className={`fixed bottom-0 left-1/2 -translate-x-1/2 max-w-[480px] min-w-[360px] px-4 py-2.5`}
         >
           <Button
             variant='primary'
--- src/features/signup/SignupGuardianIntro.tsx
+++ src/features/signup/SignupGuardianIntro.tsx
@@ -14,53 +14,13 @@ const SignupGuardianIntro = ({ onNext }: Props) => {
           alt='Signup Background'
           className='absolute inset-0 w-full h-full object-cover mx-auto'
         />
-        <div
-          className='
-          flex flex-col  
-          min-h-screen 
-          justify-between 
-          pt-11
-          z-10
-          '
-        >
-          <div
-            className='
-        flex               
-        flex-col         
-        gap-y-10           
-        px-5         
-        mt-10
-        w-full
-        max-w-[480px]
-        min-w-[360px]
-        mx-auto           
-      '
-          >
+        <div className='flex flex-col min-h-screen justify-between pt-11 z-10'>
+          <div className='flex flex-col gap-y-10 px-5 mt-10 w-full max-w-[480px] min-w-[360px] mx-auto'>
             <div className='w-full text-left'>
-              <p
-                className='
-              text-fg-primary
-              text-[22px]
-              font-extrabold
-              leading-[25px]
-              tracking-[-0.22px]
-              self-stretch
-              mb-2.5 
-            '
-              >
+              <p className='text-fg-primary text-[22px] font-extrabold leading-[25px] tracking-[-0.22px] self-stretch mb-2.5'>
                 보호자 정보를 입력해주세요
               </p>
-
-              <p
-                className='
-              text-fg-primary
-              text-[14px]
-              font-normal
-              leading-[16px]
-              tracking-[-0.14px]
-              items-stretch
-            '
-              >
+              <p className='text-fg-primary text-[14px] font-normal leading-[16px] tracking-[-0.14px] items-stretch'>
                 14세 미만은 보호자 정보를 필수 입력해주셔야 합니다
               </p>
             </div>
@@ -69,13 +29,7 @@ const SignupGuardianIntro = ({ onNext }: Props) => {
               <Squirrel />
             </div>
 
-            <div
-              className={`
-          fixed bottom-0 left-1/2 -translate-x-1/2 
-          max-w-[480px] min-w-[360px] 
-          px-4 py-5.75 z-10
-          `}
-            >
+            <div className='fixed bottom-0 left-1/2 -translate-x-1/2 max-w-[480px] min-w-[360px] px-4 py-5.75 z-10'>
               <Button
                 variant='primary'
                 onClick={onNext}
