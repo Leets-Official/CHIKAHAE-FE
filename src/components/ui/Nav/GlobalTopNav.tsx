1e44fe7b2f2ea7e19363bbfa03cae1226313f4c7
nabbang6 <hawwah03@gachon.ac.kr>
2025-08-03 02:47:16 +0900

    style: 탑 네비 텍스트 스타일 변경사항 반영

--- src/components/ui/Nav/GlobalTopNav.tsx
+++ src/components/ui/Nav/GlobalTopNav.tsx
@@ -30,7 +30,7 @@ const GlobalTopNav = ({
         items-center
         ${type === 'signup' ? 'bg-transparent' : 'bg-bg-tertiary-gray'}
         justify-between
-        max-w-[480px] min-w-[360px]
+        max-w-[480px] min-w-[360px] w-full
         z-10`}
     >
       <div className='w-[30px] flex justify-start'>
@@ -47,7 +47,7 @@ const GlobalTopNav = ({
             className={`
               flex-1 
               ${isCenter ? 'text-left' : 'text-center'} 
-              text-fg-primary body-16-eb
+              text-fg-primary head-20-eb
             `}
           >
             {message}
