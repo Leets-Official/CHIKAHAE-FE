9c31e2fcd6a03994a4e86a8c55c724e371bab956
thisminn <noblesse0308@naver.com>
2025-08-03 16:35:41 +0900

    feat:fallback 삭제 및 기본 프로필 지정 fallbackImage -> profileImage 파일명 변경

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -5,12 +5,11 @@ interface ImageProps {
   src: string;
   width?: number;
   height?: number;
-  fallbackSrc?: string; // image 파일 잘못된 경로 입력 시 보여줄 image
   className?: string; // tailwind 클래스 외부에서 유연하게 전달할 수 있도록
   onClick?: () => void;
 }
 
-const Image = ({ src, alt, width, height, fallbackSrc, className, onClick }: ImageProps) => {
+const Image = ({ src, alt, width, height, className, onClick }: ImageProps) => {
   const [currentSrc, setCurrentSrc] = useState(src);
 
   useEffect(() => {
@@ -25,11 +24,6 @@ const Image = ({ src, alt, width, height, fallbackSrc, className, onClick }: Ima
         width={width}
         height={height}
         className={`transition-transform hover:scale-105 rounded-2xl ${className ?? ''}`}
-        onError={() => {
-          if (fallbackSrc && currentSrc !== fallbackSrc) {
-            setCurrentSrc(fallbackSrc);
-          }
-        }}
         onClick={onClick}
       />
     </>
