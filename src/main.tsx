d8faed747048c7ead36d20c10cca51e7f6201ebc
nabbang6 <hawwah03@gachon.ac.kr>
2025-08-05 15:06:50 +0900

    chore: react query devtools 삭제 및 서비스워커 디버깅 코드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastProvider } from './contexts/ToastContext';
 import '@/styles/global.css';
+import '@/styles/fonts.css';
 import App from './App';
 
 const queryClient = new QueryClient();
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/firebase-messaging-sw.js')
-      .then((registration) => {
-        console.log('SW 등록 성공:', registration);
-      })
-      .catch((err) => {
-        console.error('SW 등록 실패:', err);
-      });
+    navigator.serviceWorker.register('/firebase-messaging-sw.js').catch((err) => {
+      console.error('SW 등록 실패:', err);
+    });
   });
 }
 
@@ -25,6 +20,5 @@ createRoot(document.getElementById('root')!).render(
     <ToastProvider>
       <App />
     </ToastProvider>
-    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
 );
