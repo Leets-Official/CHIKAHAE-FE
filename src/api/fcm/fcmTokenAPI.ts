4dab891dc0d7c6a59f4aafa97f2b77bf7e4f430a
nabbang6 <hawwah03@gachon.ac.kr>
2025-08-07 05:04:35 +0900

    chore: 디버깅용 console.log 삭제

--- src/api/fcm/fcmTokenAPI.ts
+++ src/api/fcm/fcmTokenAPI.ts
@@ -3,20 +3,17 @@ import api from '@/api/api';
 // FCM 토큰 등록
 export const registerFcmToken = async (fcmToken: string) => {
   const response = await api.post('/api/users/fcm-tokens', { fcmToken });
-  console.log('토큰 등록: ', fcmToken);
   return response.data;
 };
 
 // FCM 토큰 조회
 export const fetchFcmTokens = async () => {
   const response = await api.get('/api/users/fcm-tokens');
-  console.log(response);
   return response.data;
 };
 
 // FCM 토큰 삭제
 export const deleteFcmToken = async (token: string) => {
   const response = await api.delete(`/api/users/fcm-tokens/${token}`);
-  console.log(response);
   return response.data;
 };
--- src/features/alarm/registerFcmToken.ts
+++ src/features/alarm/registerFcmToken.ts
@@ -8,7 +8,6 @@ export const registerFcmTokenIfPermitted = async ({
 }: { requestPermission?: boolean } = {}) => {
   if (!isNotificationGranted()) {
     if (!requestPermission || (await Notification.requestPermission()) !== 'granted') {
-      console.log('[FCM] 알림 권한 없음.');
       return;
     }
   }
@@ -18,21 +17,15 @@ export const registerFcmTokenIfPermitted = async ({
       vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
     });
 
-    if (!token) {
-      console.warn('[FCM] 토큰을 발급받지 못했습니다.');
-      return;
-    }
+    if (!token) return;
 
     const savedToken = localStorage.getItem('fcmToken');
 
     if (!savedToken || token !== savedToken) {
       await registerFcmToken(token);
       localStorage.setItem('fcmToken', token);
-      console.log('[FCM] 토큰 등록 완료 및 저장됨');
-    } else {
-      console.log('[FCM] 이미 등록된 토큰. 재등록 생략');
     }
   } catch (err) {
-    console.error('[FCM] 토큰 발급 실패:', err);
+    console.error('FCM 토큰 발급 실패:', err);
   }
 };
